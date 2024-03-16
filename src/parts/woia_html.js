6030875b9ccfc1e30c6bc9b0078e7f1ddebe75c3
Justin Siplin <jsiplin@gmail.com>
2024-03-15 21:32:17 -0400

    Updated separation Google Doc File

--- src/parts/woia_html.js
+++ src/parts/woia_html.js
@@ -27,7 +27,7 @@ export let wdaDropdowns = `
 
 let wdaFooter = `
 <div class="Footer">
-  <p id= "wda_footer">Notes:<br>1. For Maryland Separation data up until 2019Q1, please click <a style="color:blue" href="https://docs.google.com/document/d/1UAHsLL94iaMaM1XtpqH7vcOQpiNf996EJVjHc_Xo1wk/edit?usp=sharing" target="_blank">2019Q1 Prior</a>
+  <p id= "wda_footer">Notes:<br>1. For Maryland Separation data up until 2019Q2, please click <a style="color:blue" href="https://docs.google.com/spreadsheets/d/1YnQrj3p0j1NnT0HxfP0ramIycnXsEp51JxoY_xwMfq4/edit?usp=sharing" target="_blank">2019Q2 Prior</a>
     <br>2. "TBD" represents values that are yet to be determined.
     <br>3. For Industry Metrics, "0" values in some sectors (e.g. Educational Services) represent suppressed values because they do not meet U.S. Census Bureau publication standards.
     <br>Data source: Longitudinal Employer-Household Dynamics (LEHD), last accessed on March 13, 2024<br><br></p>
